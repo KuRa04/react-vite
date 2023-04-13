f87b7c6a11e21062566aae652a5c05173fcf444c
KuRa04 <mwamkuracchi@gmail.com>
2023-04-13 15:49:16 +0900

    feat: 音声ファイルの再生処理の追加

--- src/features/check/voice/voiceForm.tsx
+++ src/features/check/voice/voiceForm.tsx
@@ -1,5 +1,27 @@
 // import { useState } from 'react';
 import { useLocation } from "react-router-dom"
+import useSound from 'use-sound';
+import wordVoiceA from '../../../audio/67s101-a.wav'
+import wordVoiceKi from '../../../audio/67s101-ki.wav'
+import wordVoiceShi from '../../../audio/67s101-shi.wav'
+import wordVoiceTa from '../../../audio/67s101-ta.wav'
+import wordVoiceNi from '../../../audio/67s101-ni.wav'
+import wordVoiceYo from '../../../audio/67s101-yo.wav'
+import wordVoiceJi from '../../../audio/67s101-ji.wav'
+import wordVoiceU from '../../../audio/67s101-u.wav'
+import wordVoiceKu from '../../../audio/67s101-ku.wav'
+import wordVoiceSu from '../../../audio/67s101-su.wav'
+import wordVoiceNe from '../../../audio/67s101-ne.wav'
+import wordVoiceHa from '../../../audio/67s101-ha.wav'
+import wordVoiceRi from '../../../audio/67s101-ri.wav'
+import wordVoiceBa from '../../../audio/67s101-ba.wav'
+import wordVoiceO from '../../../audio/67s101-o.wav'
+import wordVoiceTe from '../../../audio/67s101-te.wav'
+import wordVoiceMo from '../../../audio/67s101-mo.wav'
+import wordVoiceWa from '../../../audio/67s101-wa.wav'
+import wordVoiceTo from '../../../audio/67s101-to.wav'
+import wordVoiceGa from '../../../audio/67s101-ga.wav'
+
 import {
   Box,
   ButtonGroup,
@@ -40,6 +62,7 @@ export const VoiceFormPage = () => {
   // const toast = useToast();
   // const [step, setStep] = useState(1);
   // const [progress, setProgress] = useState(33.33);
+  const [play, { stop, pause }] = useSound(wordVoiceA);
   return (
     <>
       <Box
@@ -56,7 +79,7 @@ export const VoiceFormPage = () => {
             <Flex>
               <Button
                 onClick={() => {
-                  console.log("onClick")
+                  play()
                 }}
                 // isDisabled={step === 1}
                 colorScheme="teal"
@@ -67,7 +90,7 @@ export const VoiceFormPage = () => {
               <Button
                 // isDisabled={step === 3}
                 onClick={() => {
-                  console.log("onClick")
+                  stop()
                 }}
                 mr="5%"
                 colorScheme="teal"
