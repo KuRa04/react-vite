0f6a11302d6a63c1a6178072fd1a5ff23e9642f8
KuRa04 <mwamkuracchi@gmail.com>
2023-04-13 16:45:55 +0900

    feat: util/audio.tsに音声ファイルのimportを移動

--- src/features/check/voice/voiceForm.tsx
+++ src/features/check/voice/voiceForm.tsx
@@ -1,27 +1,7 @@
 import { useState } from 'react';
 import { useLocation } from "react-router-dom"
 import useSound from 'use-sound';
-// import wordVoiceA from '../../../audio/67s101-a.wav'
-// import wordVoiceKi from '../../../audio/67s102-ki.wav'
-// import wordVoiceShi from '../../../audio/67s103-shi.wav'
-// import wordVoiceTa from '../../../audio/67s104-ta.wav'
-// import wordVoiceNi from '../../../audio/67s105-ni.wav'
-// import wordVoiceYo from '../../../audio/67s106-yo.wav'
-// import wordVoiceJi from '../../../audio/67s107-ji.wav'
-// import wordVoiceU from '../../../audio/67s108-u.wav'
-// import wordVoiceKu from '../../../audio/67s109-ku.wav'
-// import wordVoiceSu from '../../../audio/67s110-su.wav'
-// import wordVoiceNe from '../../../audio/67s111-ne.wav'
-// import wordVoiceHa from '../../../audio/67s112-ha.wav'
-// import wordVoiceRi from '../../../audio/67s113-ri.wav'
-// import wordVoiceBa from '../../../audio/67s114-ba.wav'
-// import wordVoiceO from '../../../audio/67s115-o.wav'
-// import wordVoiceTe from '../../../audio/67s116-te.wav'
-// import wordVoiceMo from '../../../audio/67s117-mo.wav'
-// import wordVoiceWa from '../../../audio/67s118-wa.wav'
-// import wordVoiceTo from '../../../audio/67s119-to.wav'
-// import wordVoiceGa from '../../../audio/67s120-ga.wav'
-import { wordVoices } from '../../../audio';
+import { wordVoices } from '../../../util/audio';
 
 import {
   Box,
@@ -64,34 +44,12 @@ export const VoiceFormPage = () => {
   // const [step, setStep] = useState(1);
   // const [progress, setProgress] = useState(33.33);
 
-  // const wordVoices = [
-  //   wordVoiceA,
-  //   wordVoiceKi,
-  //   wordVoiceShi,
-  //   wordVoiceTa,
-  //   wordVoiceNi,
-  //   wordVoiceYo,
-  //   wordVoiceJi,
-  //   wordVoiceU,
-  //   wordVoiceKu,
-  //   wordVoiceSu,
-  //   wordVoiceNe,
-  //   wordVoiceHa,
-  //   wordVoiceRi,
-  //   wordVoiceBa,
-  //   wordVoiceO,
-  //   wordVoiceTe,
-  //   wordVoiceMo,
-  //   wordVoiceWa,
-  //   wordVoiceTo,
-  //   wordVoiceGa,
-  // ];
+  let randomIndex = Math.floor(Math.random() * wordVoices.length); // 配列のランダムなインデックスを生成
 
-  const [selectedItem, setSelectedItem] = useState<string>(''); // 選択されたアイテムをstateとして保持
+  const [selectedItem, setSelectedItem] = useState<string>(wordVoices[randomIndex]); // 選択されたアイテムをstateとして保持
   const [lastSelectedItem, setLastSelectedItem] = useState<string>(''); // 前回選択されたアイテムをstateとして保持
 
   const handleSelect = () => {
-    let randomIndex = Math.floor(Math.random() * wordVoices.length); // 配列のランダムなインデックスを生成
     while (wordVoices[randomIndex] === lastSelectedItem) { // 前回選択されたアイテムと同じ場合は再度ランダムなインデックスを生成
       randomIndex = Math.floor(Math.random() * wordVoices.length);
     }
---
+++ src/util/audio.ts
@@ -0,0 +1,43 @@
+import wordVoiceA from '../audio/67s101-a.wav'
+import wordVoiceKi from '../audio/67s102-ki.wav'
+import wordVoiceShi from '../audio/67s103-shi.wav'
+import wordVoiceTa from '../audio/67s104-ta.wav'
+import wordVoiceNi from '../audio/67s105-ni.wav'
+import wordVoiceYo from '../audio/67s106-yo.wav'
+import wordVoiceJi from '../audio/67s107-ji.wav'
+import wordVoiceU from '../audio/67s108-u.wav'
+import wordVoiceKu from '../audio/67s109-ku.wav'
+import wordVoiceSu from '../audio/67s110-su.wav'
+import wordVoiceNe from '../audio/67s111-ne.wav'
+import wordVoiceHa from '../audio/67s112-ha.wav'
+import wordVoiceRi from '../audio/67s113-ri.wav'
+import wordVoiceBa from '../audio/67s114-ba.wav'
+import wordVoiceO from '../audio/67s115-o.wav'
+import wordVoiceTe from '../audio/67s116-te.wav'
+import wordVoiceMo from '../audio/67s117-mo.wav'
+import wordVoiceWa from '../audio/67s118-wa.wav'
+import wordVoiceTo from '../audio/67s119-to.wav'
+import wordVoiceGa from '../audio/67s120-ga.wav'
+
+export const wordVoices = [
+  wordVoiceA,
+  wordVoiceKi,
+  wordVoiceShi,
+  wordVoiceTa,
+  wordVoiceNi,
+  wordVoiceYo,
+  wordVoiceJi,
+  wordVoiceU,
+  wordVoiceKu,
+  wordVoiceSu,
+  wordVoiceNe,
+  wordVoiceHa,
+  wordVoiceRi,
+  wordVoiceBa,
+  wordVoiceO,
+  wordVoiceTe,
+  wordVoiceMo,
+  wordVoiceWa,
+  wordVoiceTo,
+  wordVoiceGa,
+];
