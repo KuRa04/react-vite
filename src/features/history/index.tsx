038eacf2c704b0946aa4bc0b27a2753e4e77c48e
KuRa04 <mwamkuracchi@gmail.com>
2023-05-28 16:25:53 +0900

    feat: demoデータのグラフ作成

--- src/features/history/index.tsx
+++ src/features/history/index.tsx
@@ -11,6 +11,63 @@ import { firebase } from '../../firebase';
 
 import { getDocs, collection } from "firebase/firestore";
 
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+
 interface Answers {
   docId: string;
   gainState: number
@@ -67,6 +124,7 @@ export const HistoryPage = () => {
           <Heading as="h1" w="100%" textAlign={'left'} fontWeight="normal" mb="2%">
             聴こえチェック履歴
           </Heading>
+          <Line options={options} data={data} />
           <Button
             onClick={() => {
               getHearingData()
