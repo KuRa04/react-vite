09cb13a3329265acfcf968869da25aa3f8261452
KuRa04 <mwamkuracchi@gmail.com>
2023-05-29 15:37:06 +0900

    feat: 1000Hz

--- src/features/experiment/pureToneForm.tsx
+++ src/features/experiment/pureToneForm.tsx
@@ -16,7 +16,7 @@ export const ExperimentPureToneFormPage = () => {
   const context = new AudioContext();
   let oscillator: OscillatorNode | null = null;
   let intervalId: NodeJS.Timeout | null;
-  const frequency = 500;
+  const frequency = 1000;
   const duration = 60; // 10秒間再生
 
   const onClickStart = (gainValue: number) => {
