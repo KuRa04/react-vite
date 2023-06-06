71293634903721089ed5191b7f55e4181ce8bedc
KuRa04 <mwamkuracchi@gmail.com>
2023-06-07 08:13:35 +0900

    feat: 不要な箇所削除

--- src/features/check/puretone/index.tsx
+++ src/features/check/puretone/index.tsx
@@ -7,17 +7,6 @@ import {
 
 import { Link } from "react-router-dom";
 
-const Form1 = () => {
-  // const handleClick = () => setShow(!show);
-
-
-  return (
-    <>
-
-    </>
-  );
-};
-
 export const PureTonePage = () => {
   const siteArray = ['左', '右', '両耳']
   const hzValueArray = [250, 500, 1000, 2000, 3000, 4000, 6000, 8000, 9000, 10000, 12000, 14000, 16000]
@@ -27,7 +16,7 @@ export const PureTonePage = () => {
     '右': 'right',
     '両耳': 'both'
   };
-  
+
   return (
     <>
       <Box
